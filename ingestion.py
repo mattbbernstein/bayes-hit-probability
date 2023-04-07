c310f680df3384deafe1683cf64616f2b8275db5
Matt <mattbbernstein@gmail.com>
2023-04-07 12:54:07 -0400

    Completed EDA, moved ingestion to DB

--- ingestion.py
+++ ingestion.py
@@ -1,21 +1,29 @@
 from pybaseball import statcast, cache
 import pandas as pd
+import os
+import sqlite3
 
 DATA_FILE="project/data.csv"
+DB_FILE=r"data/data.db"
 
 def main():
     cache.enable()
+    current_folder = os.path.dirname(__file__)
+    db_path = os.path.join(current_folder, DB_FILE)
+    if not os.path.exists(os.path.dirname(db_path)):
+        os.makedirs(os.path.dirname(db_path))
+    db = connect_to_db(db_path)
     data = statcast(start_dt='2022-06-01', end_dt='2022-06-30')
-    data = data[data['type'] == 'X']
-    cols = [
-        'events',
-        'hit_distance_sc',
-        'launch_angle',
-        'launch_speed',
-        'estimated_ba_using_speedangle'
-    ]
-    data = data.filter(items=cols)
-    data.to_csv(DATA_FILE)
+    data.to_sql("statcast", db, if_exists="fail")
+
+def connect_to_db(db_file: str) -> sqlite3.Connection:
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except sqlite3.Error as e:
+        print(e)
+
+    return conn
 
 if __name__ == "__main__":
     main()
